84f330c851058340315118664a12560f0e780d6c
Nilton Hiroyuki Matsumoto <hiroyukims@hotmail.com>
2024-01-14 02:41:45 -0300

    add info

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,38 @@
-/*!
-* Start Bootstrap - Full Width Pics v5.0.6 (https://startbootstrap.com/template/full-width-pics)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-full-width-pics/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+var profile_data = {
+    name: "Nilton Hiroyuki Matsumoto",
+    skills: [{
+        title:  "",
+        description: "",
+    }],
+    softSkills: [{
+        title:  "",
+        description: "",
+    }]
+};
+
+var blog_data = {
+    post: [
+        {
+            title: "post title",
+            date: "13-01-2024",
+            subTitle: [
+                {
+                    title: "",
+                    description: ""
+                }
+            ],
+            author: "author name"
+        },
+        {
+            title: "post title",
+            date: "13-01-2024",
+            subTitle: [
+                {
+                    title: "",
+                    description: ""
+                }
+            ],
+            author: "author name"
+        }
+    ]
+}
