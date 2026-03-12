c53d5b5f1a055cebd4052cb4a2ac065cd637bde3
nhmatsumoto <hiroyukims@hotmail.com>
2026-03-12 10:09:55 +0900

    feat: Add GitHub Pages deployment workflow and configure site URL in Astro.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nhmatsumoto.github.io',
   integrations: [react(), tailwind()],
 });
